7e9a7a5f688e7654df89a913d13bd7e03334169d
Armin Eyvazi <genius2001armin@gmail.com>
2022-07-20 14:40:09 +0430

    fix relation user and fix bug

--- snappfood/app/Http/Controllers/resturantowner/OrdersController.php
+++ snappfood/app/Http/Controllers/resturantowner/OrdersController.php
@@ -5,26 +5,19 @@ namespace App\Http\Controllers\resturantowner;
 use App\Http\Controllers\Controller;
 use App\Models\Orders;
 use App\Http\Requests\UpdateOrdersRequest;
-use App\Jobs\DeleveryJob;
-use App\Jobs\PendingJob;
-use App\Jobs\SendJob;
-use App\Jobs\WaitJob;
-use App\Mail\Delevery;
-use App\Mail\Send;
-use App\Mail\Wait;
-use App\Mail\Pending;
-use App\Models\Archive;
 use App\Models\resturantowner\Restaurantowner;
 use App\Services\OrdersServies;
-use Illuminate\Support\Facades\Mail;
+use App\Models\User;
+
 
 class OrdersController extends Controller
 {
     public function create()
     {
-        $resturant_id=Restaurantowner::where('user_id',auth()->user()->id)->first();
 
-        $orders=Orders::where('restaurantowner_id',$resturant_id->id)->get();
+        $resturant_id=User::find(auth()->user()->id)->resturant->id;
+
+        $orders=Orders::where('restaurantowner_id',$resturant_id)->get();
 
         return view('resturant.orders.dashboardorders',compact('orders'));
     }

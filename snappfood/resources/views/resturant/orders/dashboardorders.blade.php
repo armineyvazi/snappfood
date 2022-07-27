45f2d4bb03a912eab4d91a3de3089c7db2a31f03
Armin Eyvazi <genius2001armin@gmail.com>
2022-07-27 14:26:24 +0430

    Add dashboard order

--- snappfood/resources/views/resturant/orders/dashboardorders.blade.php
+++ snappfood/resources/views/resturant/orders/dashboardorders.blade.php
@@ -6,58 +6,102 @@
 
 <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
 
-
     @forelse ($orders as $order)
-{{-- {{ dd($orders)->id }} --}}
 
+                <div class=" p-4  bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-red-700">
+                    <div class="flex justify-between items-center mb-4">
+                        <p class="text-sm font-bold leading-none text-gray-900 dark:text-white">Name:{{ $order->name }}</p>
+                        <p class="text-sm font-bold leading-none text-gray-900 dark:text-white">Phone:{{ $order->phone }}</p>
+                        <p class="text-sm font-bold leading-none text-gray-900 dark:text-white">Email:{{ $order->email }}</p>
+                            <form action="{{ url('restaurantowners/orders') }}" method="post">
+                                @csrf
+                                <input type="hidden" name="id" value="{{ $order->id }}">
+                                <input type="hidden" name="email" value="{{ $order->email }}">
 
-<div class=" p-4  bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-red-700">
-    <div class="flex justify-between items-center mb-4">
-        <p class="text-sm font-bold leading-none text-gray-900 dark:text-white">Name:{{ $order->name }}</p>
-        <p class="text-sm font-bold leading-none text-gray-900 dark:text-white">Phone:{{ $order->phone }}</p>
-        <p class="text-sm font-bold leading-none text-gray-900 dark:text-white">Email:{{ $order->email }}</p>
-            <form action="{{ url('restaurantowners/orders') }}" method="post">
-                @csrf
-                <input type="hidden" name="id" value="{{ $order->id }}">
-                <input type="hidden" name="email" value="{{ $order->email }}">
-
-                    <select name="orders_status" onchange="this.form.submit()" class="w-40 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
-                   <option  value="pending">Pending</option>
-                    <option  value="wait">Wait</option>
-                    <option value="send">Send</option>
-                    <option value="delivered">Delivered</option>
-                    </select>
-            </form>
-   </div>
-   <div class="flow-root">
-        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
-
-            <li class="sm:py-4">
-
-                <div class="items-center text-base font-semibold text-gray-900 dark:text-white">
-                    foodsname:{{ $order->foods_name }}
+                                    <select name="orders_status" onchange="this.form.submit()" class="w-40 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                <option  value="pending">Pending</option>
+                                    <option  value="wait">Wait</option>
+                                    <option value="send">Send</option>
+                                    <option value="delivered">Delivered</option>
+                                    </select>
+                            </form>
                 </div>
+                <div class="flow-root">
+                        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
 
-                <div class="items-center text-base font-semibold text-gray-900 dark:text-white">
-                    foodscount:{{ $order->count }}
+                            <li class="sm:py-4">
+
+                                <div class="items-center text-base font-semibold text-gray-900 dark:text-white">
+                                    foodsname:{{ $order->foods_name }}
+                                </div>
+
+                                <div class="items-center text-base font-semibold text-gray-900 dark:text-white">
+                                    foodscount:{{ $order->count }}
+                                </div>
+
+                                <div class="items-cen<div class=" p-4  bg-white rounded-lg border shadow-md sm:p-8 dark:bg-gray-800 dark:border-red-700">
+                                    <div class="flex justify-between items-center mb-4">
+                                        <p class="text-sm font-bold leading-none text-gray-900 dark:text-white">Name:{{ $order->name }}</p>
+                                        <p class="text-sm font-bold leading-none text-gray-900 dark:text-white">Phone:{{ $order->phone }}</p>
+                                        <p class="text-sm font-bold leading-none text-gray-900 dark:text-white">Email:{{ $order->email }}</p>
+                                            <form action="{{ url('restaurantowners/orders') }}" method="post">
+                                                @csrf
+                                                <input type="hidden" name="id" value="{{ $order->id }}">
+                                                <input type="hidden" name="email" value="{{ $order->email }}">
+
+                                                    <select name="orders_status" onchange="this.form.submit()" class="w-40 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                                                   <option  value="pending">Pending</option>
+                                                    <option  value="wait">Wait</option>
+                                                    <option value="send">Send</option>
+                                                    <option value="delivered">Delivered</option>
+                                                    </select>
+                                            </form>
+                                   </div>
+                                   <div class="flow-root">
+                                        <ul role="list" class="divide-y divide-gray-200 dark:divide-gray-700">
+
+                                            <li class="sm:py-4">
+
+                                                <div class="items-center text-base font-semibold text-gray-900 dark:text-white">
+                                                    foodsname:{{ $order->foods_name }}
+                                                </div>
+
+                                                <div class="items-center text-base font-semibold text-gray-900 dark:text-white">
+                                                    foodscount:{{ $order->count }}
+                                                </div>
+
+                                                <div class="items-center text-base font-semibold text-gray-900 dark:text-white">
+                                                    sum:{{ $order->sum }}
+                                                </div>
+                                                <div class="items-center text-base font-semibold text-gray-900 dark:text-white">
+                                                    Orderstatus:{{ $order->orders_status }}
+                                                </div>
+
+                                            </li>
+
+                                        </ul>
+                                   </div>
+                                </div>
+                                @empty
+                                <h2 class="text-center font-bold font-sans text-xl">There is no Order For your Restaurant !</h2>
+                                 @endforelse
+                                </div>
+                                ter text-base font-semibold text-gray-900 dark:text-white">
+                                    sum:{{ $order->sum }}
+                                </div>
+                                <div class="items-center text-base font-semibold text-gray-900 dark:text-white">
+                                    Orderstatus:{{ $order->orders_status }}
+                                </div>
+
+                            </li>
+
+                        </ul>
                 </div>
-
-                <div class="items-center text-base font-semibold text-gray-900 dark:text-white">
-                    sum:{{ $order->sum }}
                 </div>
-                <div class="items-center text-base font-semibold text-gray-900 dark:text-white">
-                    Orderstatus:{{ $order->orders_status }}
+                @empty
+                <h2 class="text-center font-bold font-sans text-xl">There is no Order For your Restaurant !</h2>
+                @endforelse
                 </div>
-
-            </li>
-
-        </ul>
-   </div>
-</div>
-@empty
-<h2 class="text-center font-bold font-sans text-xl">There is no Order For your Restaurant !</h2>
- @endforelse
-</div>
 
             </div>
         </div>

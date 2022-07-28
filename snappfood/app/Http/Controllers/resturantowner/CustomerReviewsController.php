a6c40d5bdf2a883609415bc3d47060df6eada8c8
Armin Eyvazi <genius2001armin@gmail.com>
2022-07-28 18:03:42 +0430

    Add Customer Reviews

--- snappfood/app/Http/Controllers/resturantowner/CustomerReviewsController.php
+++ snappfood/app/Http/Controllers/resturantowner/CustomerReviewsController.php
@@ -8,28 +8,64 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Models\admin\FoodsCategory;
 use App\Http\Requests\UpdateCommentRequest;
+use App\Models\resturantowner\ResturantFoods;
 
 class CustomerReviewsController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
+        $comment=[];
+
+        $nameFood=[];
+
         $resturant_id=User::find(auth()->user()->id)->resturant->id;
 
-        $category=FoodsCategory::paginate();
+        $category=ResturantFoods::paginate();
 
-        $comment=Comment::where('restaurantowner_id',$resturant_id)?->get();
+        $search=$request->all()['food'] ?? false;
 
+        if(($search)==='000'){//bad score
+
+            $comment=Comment::where('score', '<=', 2)->where('restaurantowner_id', $resturant_id)?->get();
+        }
+        if(($search)==='00'){//good score
+
+            $comment=Comment::where('score','>=',3)->where('restaurantowner_id',$resturant_id)?->get();
+        }
+        if(($search)>0 ){
+
+           $nameFood=$nameFood=ResturantFoods::where('id',$search)->where('restaurantowner_id',$resturant_id)?->first()->name ?? false;
+
+           $nameFood ? $comment=Comment::where('foodName','REGEXP',$nameFood.',.+')->where('restaurantowner_id',$resturant_id)?->get():$comment=[];
+
+        }
+        if(($search)=='all'){
+            
+            $comment=Comment::where('restaurantowner_id',$resturant_id)?->get();
+        }
         return view('resturant.comment.commentindex',compact('comment','category'));
     }
 
     public function update(UpdateCommentRequest $request)
     {
+        $report=$request->validated()['report']??null;
+
         $id=$request->validated()['id'];
-        $message=$request->validated()['reply'];
+
+        $message=$request->validated()['reply']??null;
+
         $data=[
             'answer'=>$message,
         ];
-        comment::find($id)->update($data);
-        return redirect('restaurantowners/customerreviews')->with(['message'=>'Answer is saved']);
+        if (!is_null($report))
+        {
+            comment::find($id)->update(['report'=>true]);
+            return redirect('restaurantowners/customerreviews')->with(['message'=>'Comment is report']);
+        }
+        if(!is_null($message))
+        {
+            comment::find($id)->update($data);
+            return redirect('restaurantowners/customerreviews')->with(['message'=>'Answer is saved']);
+        }
     }
 }
